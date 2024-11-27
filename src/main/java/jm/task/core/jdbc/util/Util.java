34e8cf1d8850f46d4816877497890e709663ebb1
Andrey-true <andreymgn184123@gmail.com>
2024-11-27 21:42:52 +0500

    Задача решена.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASWORD = "postgres";
+
+    public static Connection getConnection () {
+        try {
+            return DriverManager.getConnection(URL, USER, PASWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
